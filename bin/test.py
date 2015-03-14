c25178b81d35f86af143c1c2e6cb80f6885614ef
François-Xavier Choinière <fx@efficks.com>
2015-03-14 14:47:57 -0400

    Version fonctionelle. Manque css

---
+++ bin/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+import vulcanize
+
+if __name__ == '__main__':
+    unittest.main()
--- bin/vulcanize.py
+++ bin/vulcanize.py
@@ -5,17 +5,36 @@ import os
 import sys
 import codecs
 import argparse
+import re
 from HTMLParser import HTMLParser
 
 class Vulcanizer(HTMLParser):
-	def __init__(self, output, strip):
+	def __init__(self, output, strip, path):
 		HTMLParser.__init__(self)
 		self.__strip = strip
 		self.__output = output
 		self.__linkedData = u""
+		self.__path = path
+		self.__currentFileDir = None
+		self.__inscript = False
+
+	def process(self, documentPath):
+		self.__currentFileDir = os.path.dirname(documentPath)
+		with codecs.open(documentPath, encoding='utf-8', mode="r") as fh:
+			# TODO: Read the file content by chunk to prevent memory consumption
+			content = fh.read()
+			self.feed(content)
+	
+	def __handleLink(self, href):
+		linkPath = os.path.normpath(os.path.join(self.__currentFileDir, href))
+		vulcanizer = Vulcanizer(self.__output, self.__strip, self.__path)
+		vulcanizer.process(linkPath)
 	
 	def __handle_genericstart(self, tag, attrs):
 		if tag == "link":
+			a = dict(attrs)
+			if a["rel"] == "import" and "href" in a:
+				self.__handleLink(a["href"])
 			return False
 		else:
 			self.__output.write("<%s" % tag)
@@ -24,17 +43,22 @@ class Vulcanizer(HTMLParser):
 				attrList.append("%s=\"%s\"" % (name, value))
 			if len(attrList) > 0:
 				self.__output.write(" %s" % " ".join(attrList))
+
 		return True
 	
 	def handle_starttag(self, tag, attrs):
 		if self.__handle_genericstart(tag, attrs):
 			self.__output.write(">")
+		if tag == "script":
+			self.__inscript = True
 	
 	def handle_endtag(self, tag):
 		if tag == "link":
 			pass
 		else:
 			self.__output.write("</%s>" % tag)
+		if tag == "script":
+			self.__inscript = False
 	
 	def handle_startendtag(self, tag, attrs):
 		if self.__handle_genericstart(tag, attrs):
@@ -42,6 +66,12 @@ class Vulcanizer(HTMLParser):
 	
 	def handle_data(self, data):
 		if self.__strip:
+			if self.__inscript:
+				data = data.replace("\n","")
+				data = data.replace("\r","")
+				data = re.sub("/\*.*\*/", "", data)
+				data = re.sub(" +(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", "", data)
+				data = re.sub("\t+(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", "", data)
 			self.__output.write("%s" % data.strip())
 		else:
 			self.__output.write("%s" % data)
@@ -69,19 +99,12 @@ def getArguments():
 	parser.add_argument("inputfiles", metavar="HTMLFILE", nargs=1, help="HTML file to process")
 	parser.add_argument("-o", "--output", metavar="OUTPUT", nargs="?", help="Path to the output file. Standard output if not used")
 	parser.add_argument("-s", "--strip", action="store_true", help="Remove comments and empty text nodes")
-	parser.add_argument("-p", "--path", metavar="PATH", action="append", help="Path to search for relative path in import tag")
+	parser.add_argument("-p", "--path", metavar="PATH", action="append", default=[], help="Path to search for relative path in import tag")
 	parser.add_argument("-M", "--dependencies", action="store_true", help="Instead of outputting the vulcanized data, output relative path to imported document splitted by a new line")
 	
 	args = parser.parse_args()
 	return args
 
-def processDocument(documentPath, output, strip):
-	with codecs.open(documentPath, encoding='utf-8', mode="r") as fh:
-		# TODO: Read the file content by chunk to prevent memory consumption
-		content = fh.read()
-		vulcanizer = Vulcanizer(output, strip)
-		vulcanizer.feed(content)
-	
 def main():
 	args = getArguments()
 	
@@ -97,7 +120,8 @@ def main():
 		output = sys.stdout
 	
 	for inputFile in args.inputfiles:
-		processDocument(inputFile, output, args.strip)
+		vulcanizer = Vulcanizer(output, args.strip, args.path)
+		vulcanizer.process(inputFile)
 
 if __name__ == "__main__":
 	main()
